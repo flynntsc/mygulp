c0d0c12d1076c8228c59f1f78e1ee07aa2d7c9c4
flynntsc <flynntsc@qq.com>
2015-12-05 10:43:12 +0800

    edit path

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,26 @@ var gulp = require('gulp'),
 			'gulp-minify-css': 'mincss'
 		}
 	}),
-	path = {
-		HTML: 'html/**/*.html',
-		SASS: 'src/sass/*.scss',
-		CSS: 'src/css/**/*.css',
-		IMG: 'src/images/**/*',
-		JS: 'src/js/**/*.js'
+	PATH = {
+		Html: 'html/**/*.html',
+		Sass: 'src/sass/*.scss',
+		CssTo: 'dist/css',
+		Js: 'src/js/**/*.js',
+		JsTo: 'dist/js',
+		Img: 'src/images/**/*',
+		ImgTo: 'dist/images'
 	};
 
 // sass编译css
-gulp.task('fnSass', function () {
-	gulp.src(path.SASS)
+gulp.task('fnSass', function() {
+	gulp.src(PATH.Sass)
 		// 编译scss
 		// outputStyle:nested/expanded/compact/compressed
 		.pipe(plugins.sass({outputStyle: 'expanded'}).on('error', plugins.sass.logError))
 		// 加前缀
         .pipe(plugins.autoprefixer({browsers: ['last 2 versions'],cascade: false}))
         // 输出
-		.pipe(gulp.dest('dist/css'))
+		.pipe(gulp.dest(PATH.CssTo))
 		// 压缩
 		.pipe(plugins.mincss())
 		// 重命名
@@ -33,37 +35,37 @@ gulp.task('fnSass', function () {
 			// path.extname = '.css';
 		}))
 		// 再输出
-		.pipe(gulp.dest('dist/css'))
+		.pipe(gulp.dest(PATH.CssTo))
 		// 刷新
         .pipe(browserSync.stream());
 });
 
 //Js编译
 gulp.task('fnJs',function() {
-	gulp.src(path.JS)
+	gulp.src(PATH.Js)
 		.pipe(plugins.babel({
 			presets: ['es2015']
 		}))
         // 输出
-		// .pipe(gulp.dest('dist/js'))
+		// .pipe(gulp.dest(PATH.JsTo)
 		// 压缩
 		// .pipe(plugins.uglify())
 		// 重命名
-		.pipe(plugins.rename(function(path) {
-			path.basename += '.min';
-			// path.extname = '.css';
+		.pipe(plugins.rename(function(PATH) {
+			PATH.basename += '.min';
+			// PATH.extname = '.css';
 		}))
         // 输出
-		.pipe(gulp.dest('dist/js'));
+		.pipe(gulp.dest(PATH.JsTo));
 });
 
 //处理图片
 gulp.task('fnImg',function() {
-	gulp.src(path.IMG)
+	gulp.src(PATH.Img)
 		.pipe(plugins.imagemin({
 			progressive: true
 		}))
-		.pipe(gulp.dest('dist/images'));
+		.pipe(gulp.dest(PATH.ImgTo));
 });
 
 //监视变化
@@ -73,17 +75,17 @@ gulp.task('fnAuto',function() {
 	});
 
     // 打印更改路径与事件
-    // gulp.watch([path.HTML,path.JS,path.SASS,path.CSS,path.IMG], function(event) {
-    // 	console.info(event.path);
+    // gulp.watch([PATH.Html,PATH.Js,PATH.Sass,PATH.Img], function(event) {
+    // 	console.info(event.PATH);
 
     // 	// changed、added、deleted、renamed
     // 	console.info(event.type);
     // });
 
-	gulp.watch(path.SASS,['fnSass']);
-	gulp.watch(path.JS,['fnJs']);
-    // gulp.watch(path.IMG,['fnImg']);
-	gulp.watch([path.HTML,path.JS]).on('change',browserSync.reload);
+	gulp.watch(PATH.Sass,['fnSass']);
+	gulp.watch(PATH.Js,['fnJs']);
+    // gulp.watch(PATH.Img,['fnImg']);
+	gulp.watch([PATH.Html,PATH.Js]).on('change',browserSync.reload);
 });
 // 默认运行任务
 gulp.task('default',['fnAuto']);
