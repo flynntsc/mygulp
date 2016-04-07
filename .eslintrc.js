ff9a82621f651bc0cfb50f699d30c3b2cf13a652
flynntsc <flynntsc@qq.com>
2016-04-07 16:04:46 +0800

    add eslint-config-google

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,3 @@
 module.exports = {
-    "rules": {
-        "indent": [
-            2,
-            "tab"
-        ],
-        "quotes": [
-            2,
-            "single"
-        ],
-        "linebreak-style": [
-            2,
-            "unix"
-        ],
-        "semi": [
-            2,
-            "always"
-        ]
-    },
-    "env": {
-        "es6": true,
-        "node": true,
-        "browser": true
-    },
-    "extends": "eslint:recommended",
-    "ecmaFeatures": {
-        "jsx": true,
-        "experimentalObjectRestSpread": true
-    },
-    "plugins": [
-        "react"
-    ]
+    "extends": "google"
 };
